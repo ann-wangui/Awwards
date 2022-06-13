573afddae98e377d2da11c4ad40f86293e2ea509
ann-wangui <wanguia822@gmail.com>
2022-06-13 11:57:35 +0300

    commit all changes

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='anne', password='wangui22')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='anne')
+        self.post = Post.objects.create(id=1, title='test post', photo='/media/images/home-img.png', description='the one',
+                                        user=self.user, url='kelord-blog.herokuapp.com/')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) < 1)
+
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='ann')
+        self.post = Post.objects.create(id=1, title='test post', photo='/media/images/home-img.png', description='the one',
+                                        user=self.user, url='kelord-blog.herokuapp.com/')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+ 
